2549b46cb581c19e57ddbc11fcb9910bf5f1637b
Hao Wu <skyw@nvidia.com>
2025-10-21 10:55:49 -0700

    update ci test script
    Signed-off-by: Hao Wu <skyw@nvidia.com>

--- tests/ci/L1_Tests_GPU.sh
+++ tests/ci/L1_Tests_GPU.sh
@@ -17,7 +17,6 @@ export TORCH_ALLOW_TF32_CUBLAS_OVERRIDE=0
 error=0
 python tests/test_muon_utils.py || error=1
 python tests/test_orthogonalized_optimizer.py || error=1
-python tests/test_soap_functions.py || error=1
 python tests/test_soap_utils.py || error=1
 python tests/test_soap.py || error=1
 python tests/test_scalar_optimizers.py --device=cuda || error=1
