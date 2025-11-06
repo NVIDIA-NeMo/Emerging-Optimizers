ba89adb77e0232ee8b1558d2e4f31424d71961d9
Hao Wu <skyw@nvidia.com>
2025-11-06 12:14:19 -0800

    export module in init
    Signed-off-by: Hao Wu <skyw@nvidia.com>

--- emerging_optimizers/__init__.py
+++ emerging_optimizers/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from emerging_optimizers import mixin
 from emerging_optimizers.package_info import (
     __contact_emails__,
     __contact_names__,
