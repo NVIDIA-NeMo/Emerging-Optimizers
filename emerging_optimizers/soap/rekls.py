8c8c6b01e4cae8ca3942ec63f8f9dc0fa4a15b8a
Hao Wu <skyw@nvidia.com>
2026-02-09 19:18:54 -0800

    Revert import logic change, needs more consideration to make big change
    Signed-off-by: Hao Wu <skyw@nvidia.com>

--- emerging_optimizers/soap/rekls.py
+++ emerging_optimizers/soap/rekls.py
@@ -17,14 +17,14 @@ from torch.optim.optimizer import ParamsT
 
 from emerging_optimizers import mixin as opt_mixin
 from emerging_optimizers import registry
-from emerging_optimizers.soap import soap
+from emerging_optimizers.soap.soap import SOAP
 
 
 __all__ = ["REKLS"]
 
 
 @registry.register_optimizer("rekls")
-class REKLS(soap.SOAP):
+class REKLS(SOAP):
     """REKLS (Realtime Eigen Kullback-Leibler Soap) optimizer.
 
     REKLS is a variant of SOAP that uses the up to date eigenbasis calculated by Eigen decomposition. It is
