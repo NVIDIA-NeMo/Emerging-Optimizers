5593b46bbc9da2e9441a8adfe9e89d7efe751d56
Tim Lau <timlautk@gmail.com>
2026-02-12 20:49:02 -0800

    Modify docstring in polargrad.py
    Signed-off-by: Tim Lau <timlautk@gmail.com>

--- emerging_optimizers/orthogonalized_optimizers/polargrad.py
+++ emerging_optimizers/orthogonalized_optimizers/polargrad.py
@@ -54,10 +54,8 @@ class PolarGrad(OrthogonalizedOptimizer):
         coefficient_type: The type of coefficient set to use for the Newton-Schulz iteration. Can be one of
             ["simple", "quintic", "polar_express"].
         num_ns_steps: The number of iteration steps to use in the Newton-Schulz iteration.
-        scale_mode: The type of scale factor to use for the update. Defaults to "nuclear_norm" style scaling.
         extra_scale_factor: The additional scale factor to use for the update. Setting it to 0.2 can closely match
             the update RMS norm of AdamW as suggested by https://arxiv.org/abs/2502.16982.
-        use_syrk: Whether to use the Triton kernel for the Newton-Schulz iteration.
     """
 
     def __init__(
