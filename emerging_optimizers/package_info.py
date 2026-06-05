df60d7537c8f5468f662b69dd16c622b0704461f
Charlie Truong <chtruong@nvidia.com>
2026-06-05 12:48:59 -0500

    Initial pypi
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- emerging_optimizers/package_info.py
+++ emerging_optimizers/package_info.py
@@ -14,9 +14,9 @@
 
 
 MAJOR = 0
-MINOR = 4
+MINOR = 0
 PATCH = 0
-PRE_RELEASE = "a0"
+PRE_RELEASE = ""
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
